ea8bf98e250147819b6851f53834c0642e4a4975
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-08 11:41:50 +0300

    updated index with the left menu  export

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,7 @@
 export {default as Header} from './Header';
 export {default as Footer} from './Footer';
 export {default as Sidebar} from './Sidebar';
+export {default as LeftMenu} from './LeftMenu';
 export {default as Button} from './Button';
 export {default as Alert} from './Alert';
 export {default as UserProfile} from './ProfileMenu';
