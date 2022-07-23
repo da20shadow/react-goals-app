7d497a6f26389ac20884234d857a43200c90f2e4
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-23 16:06:34 +0300

    Added colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  darkMode: 'class',
   theme: {
+    colors: {
+      'white': '#FFFFFF',
+      'dark-green': '#14532d',
+      'green': '#15803d',
+      'light-green': '#bbf7d0',
+      'neutral-green': '#dcfce7',
+      'dark-red': '#7f1d1d',
+      'red': '#b91c1c',
+      'light-red': '#fecaca',
+      'neutral-red': '#fee2e2',
+      'dark-blue': '#0c4a6e',
+      'blue': '#075985',
+      'light-blue': '#bae6fd',
+      'neutral-blue': '#dbeafe',
+      'dark-orange': '#9a3412',
+      'orange': '#ca8a04',
+      'light-orange': '#fed7aa',
+      'neutral-orange': '#ffedd5',
+      'dark-gray': '#1f2937',
+      'gray': '#4b5563',
+      'secondary-gray': '#9ca3af',
+      'light-gray': '#e8e8e8',
+      'neutral-gray': '#f3f4f6',
+      'main-bg': '#FAFBFB',
+      'main-dark-bg': '#20232A',
+      'secondary-dark-bg': '#33373E',
+      'half-transparent': 'rgba(0, 0, 0, 0.5)',
+    },
     extend: {
 
     },
