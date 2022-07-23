93b6d36318ec192f2d5644db4f41f773bde5ace5
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-23 16:07:16 +0300

    updated app.js structure

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
-import {Header, Footer, Sidebar} from "./Components";
+import {Header, Footer, Sidebar, LeftMenu} from "./Components";
 import {NavLink, Route, Routes} from "react-router-dom";
-import {Account, Home, Login, NotFound, Register} from "./Pages";
+import {Account, Goal, GoalsList, Home, Login, NotFound, Register} from "./Pages";
 import {useStateContext} from "./Contexts/ContextProvider";
-import {useState} from "react";
+import './Components/Sidebar/Sidebar.css';
+import {logout} from "./Features/user/userSlice";
+import {useDispatch} from "react-redux";
 
 function App() {
-    const {activeSidebar} = useStateContext();
+    const dispatch = useDispatch();
+    const {user,isLogged,activeSidebar, setActiveSidebar} = useStateContext();
 
-    const [showSidebar, setShowSidebar] = useState(false);
-
+    const expires = new Date(user.expires * 1000);
+    const date = new Date();
+    if (expires < date){
+        dispatch(logout());
+    }
+    console.log('Expires: ',expires)
+    console.log('Now: ',date)
+    console.log('Expires Minutes: ',expires.getMinutes())
+    console.log('Now Minutes: ',date.getMinutes())
+    console.log(expires < date)
     return (
         <>
+            <div className="flex relative dark:bg-main-dark-bg">
 
-            <Header/>
-            <Sidebar />
+                {/*SideBar*/}
+                <div className={`${activeSidebar 
+                    ? 'w-full sm:w-72 fixed bg-white border-r border-light-blue shadow-xl shadow-light-blue z-50' 
+                    : 'w-0 ' } ease-in-out duration-300 dark:bg-secondary-dark-bg`}>
+                    <LeftMenu/>
+                </div>
 
-            <Routes>
-                <Route path={'/'} element={<Home/>}/>
-                <Route path={'/login'} element={<Login/>}/>
-                <Route path={'/register'} element={<Register/>}/>
-                <Route path={'/account'} element={<Account/>}/>
-                <Route path={'*'} element={<NotFound/>}/>
-            </Routes>
+                {/*Header*/}
+                <div className={`
+                    ${activeSidebar ? 'md:ml-72' : ''} 
+                        bg-main-bg dark:bg-main-dark-bg min-h-screen w-full`}
+                >
+                    <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+                        <Header/>
+                    </div>
+                    <div>
+                        <Routes>
+                            <Route path={'/'} element={<Home/>}/>
+                            <Route path={'/login'} element={isLogged ? <Account/> : <Login/>}/>
+                            <Route path={'/register'} element={isLogged ? <Account/> : <Register/>}/>
+                            <Route path={'/account'} element={isLogged ? <Account/> : <Login/>}/>
+                            <Route path={'/goals'} element={isLogged ? <GoalsList/> : <Login/>}/>
+                            <Route path={'/goal/:goal_id'} element={isLogged ? <Goal/> : <Login/>}/>
+                            <Route path={'*'} element={<NotFound/>}/>
+                        </Routes>
+                    </div>
+                    <Footer/>
+                </div>
 
-            <Footer/>
+            </div>
+
         </>
     );
 }
+
+
 
 export default App;
