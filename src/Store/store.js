0f1d6ca4fa6bee1af8f3b990756ae717af44f5c1
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-23 16:56:23 +0300

    Created redux store

---
+++ src/Store/store.js
@@ -0,0 +1,8 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import userReducer from '../Features/user/userSlice';
+//
+// export default configureStore({
+//     reducer: {
+//         login: userReducer
+//     }
+// })
