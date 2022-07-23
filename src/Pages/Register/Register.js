e5d5a136676a41e63b994a8b99e37224c77f7445
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-23 16:55:25 +0300

    register page updated

--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,9 +1,9 @@
 function Register(){
     return(
         <>
-        <header className={'bg-white shadow'}>
+        <header className={'bg-white shadow mt-16 md:mt-0'}>
             <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <h1 className="text-3xl font-bold text-center text-gray-900">Register</h1>
+                <h1 className="text-3xl font-bold text-center text-blue">Register</h1>
             </div>
         </header>
 
